eaef49d5ca49b84a72be74c6a826e42ba842d891
Shane Geary <stg4462@comcast.net>
2020-02-03 19:46:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,10 @@ class Song
     song 
   end 
   
+  def self.find_by_name
+    
+  end 
+  
   def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end 
